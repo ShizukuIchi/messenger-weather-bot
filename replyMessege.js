bc666286778989aa2f18248df9d4704c54fb1d18
shizuku <eddy540511@yahoo.com.tw>
2017-11-28 15:55:38 +0800

    87

---
+++ replyMessege.js
@@ -0,0 +1,29 @@
+const getRandomInt = (min, max) => (
+  Math.floor(Math.random()*(max-min) +min)
+)
+
+function getRandomReply(){
+  messages = [
+    '蛤?',
+    '87',
+    '密P',
+    '別密ㄌ',
+    '8888888',
+    '誰派你來ㄉ',
+    '太閒?',
+    '再來R',
+    '再來RRRRRRRRRRR',
+    '再來R!!!!!!!!',
+    '互相傷害阿!',
+    '此處無銀三百兩',
+    '安安你好',
+    '安安，很高興為您服務',
+    '我4最棒ㄉ回話ㄐ器人',
+    '我比較ㄎi'
+  ]
+  return messages[getRandomInt(0, messages.length)]
+}
+
+module.exports = {
+  getRandomReply: getRandomReply
+}
