36f5e761eea917fd12a97cbabc57d3ee037142b5
shizuku <eddy540511@yahoo.com.tw>
2017-11-28 15:59:04 +0800

    87

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const getRandomMessage = require('./replyMessage.js')
+const getRandomReply = require('./replyMessage.js').getRandomReply
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -35,7 +35,7 @@ app.post('/webhook/', function (req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-			sendTextMessage(sender, getRandomMessage())
+			sendTextMessage(sender, getRandomReply())
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
--- replyMessege.js
+++ replyMessege.js
@@ -1,6 +1,6 @@
-const getRandomInt = (min, max) => (
-  Math.floor(Math.random()*(max-min) +min)
-)
+function getRandomInt(min, max){
+  return Math.floor(Math.random()*(max-min) +min)
+}
 
 function getRandomReply(){
   messages = [
