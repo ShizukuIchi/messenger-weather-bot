9e3ffebd8972bf6b3ed6da9ccdd919638fcfc4b0
shizuku <eddy540511@yahoo.com.tw>
2017-12-04 04:33:45 +0800

    weather

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const replyMessage = require('./replyMessage.js')
-const getWeather = require('./getWeather.js')
+const getWeather = require('./getWeather.js').getWeather
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
